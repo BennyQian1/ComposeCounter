e0733486172bd1ee44e7786d874d933fcbf372b5
BennyQian1 <tuq28313@temple.edu>
2025-04-23 15:57:06 -0400

    -WORKING COUNTER

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,12 +47,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+
+    var count by rememberSaveable { mutableIntStateOf(0) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedButton (
+            onClick = { count += 1 },
+            modifier = Modifier.padding(bottom = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_up_24),
+                contentDescription = "Drawable Button",
+                modifier = Modifier.size(50.dp)
+            )
+        }
         Text(
-            text = "0",
+            text = "$count",
             fontSize = 192.sp
         )
+
+        OutlinedButton (
+            onClick = { count -= 1 },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
+                contentDescription = "Drawable Button",
+                modifier = Modifier.size(50.dp)
+            )
+        }
     }
 }
 
